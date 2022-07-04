de91108e05297e5b53e4fd5cf46be4f5ba96b347
jaeik-kim <jaeik1.kim@gmail.com>
2022-07-04 17:49:45 +0800

    Only support WBTCUSD, BTCBUSD, XTUSD, ETHUSD, BNBUSD

--- src/median.sol
+++ src/median.sol
@@ -26,18 +26,13 @@ contract MedianXTUSD is Median {
     }
 }
 
-contract MedianXBTCUSD is Median {
-    constructor() Median("BTCUSD") public {
+contract MedianWBTCUSD is Median {
+    constructor() Median("WBTCUSD") public {
     }
 }
 
-contract MedianXETHUSD is Median {
-    constructor() Median("ETHUSD") public {
-    }
-}
-
-contract MedianXUSDTUSD is Median {
-    constructor() Median("USDTUSD") public {
+contract MedianBTCBUSD is Median {
+    constructor() Median("BTCBUSD") public {
     }
 }
 
@@ -46,112 +41,7 @@ contract MedianETHUSD is Median {
     }
 }
 
-contract MedianBATUSD is Median {
-    constructor() Median("BATUSD") public {
-    }
-}
-
-contract MedianWBTCUSD is Median {
-    constructor() Median("BTCUSD") public {
-    }
-}
-
-contract MedianREPUSD is Median {
-    constructor() Median("REPUSD") public {
-    }
-}
-
-contract MedianZRXUSD is Median {
-    constructor() Median("ZRXUSD") public {
-    }
-}
-
-contract MedianOMGUSD is Median {
-    constructor() Median("OMGUSD") public {
-    }
-}
-
-contract MedianDGDUSD is Median {
-    constructor() Median("DGDUSD") public {
-    }
-}
-
-contract MedianGNTUSD is Median {
-    constructor() Median("GNTUSD") public {
-    }
-}
-
-contract MedianUSDCUSD is Median {
-    constructor() Median("USDCUSD") public {
-    }
-}
-
-contract MedianTUSDUSD is Median {
-    constructor() Median("TUSDUSD") public {
-    }
-}
-
-contract MedianKNCUSD is Median {
-    constructor() Median("KNCUSD") public {
-    }
-}
-
-contract MedianMANAUSD is Median {
-    constructor() Median("MANAUSD") public {
-    }
-}
-
-contract MedianUSDTUSD is Median {
-    constructor() Median("USDTUSD") public {
-    }
-}
-
-contract MedianPAXUSDUSD is Median {
-    constructor() Median("PAXUSDUSD") public {
-    }
-}
-
-contract MedianCOMPUSD is Median {
-    constructor() Median("COMPUSD") public {
-    }
-}
-
-contract MedianLRCUSD is Median {
-    constructor() Median("LRCUSD") public {
-    }
-}
-
-contract MedianLINKUSD is Median {
-    constructor() Median("LINKUSD") public {
-    }
-}
-
-contract MedianBALUSD is Median {
-    constructor() Median("BALUSD") public {
-    }
-}
-
-contract MedianYFIUSD is Median {
-    constructor() Median("YFIUSD") public {
-    }
-}
-
-contract MedianGUSDUSD is Median {
-    constructor() Median("GUSDUSD") public {
-    }
-}
-
-contract MedianRENBTCUSD is Median {
-    constructor() Median("RENBTCUSD") public {
-    }
-}
-
-contract MedianUNIUSD is Median {
-    constructor() Median("UNIUSD") public {
-    }
-}
-
-contract MedianAAVEUSD is Median {
-    constructor() Median("AAVEUSD") public {
+contract MedianBNBUSD is Median {
+    constructor() Median("BNBUSD") public {
     }
 }
